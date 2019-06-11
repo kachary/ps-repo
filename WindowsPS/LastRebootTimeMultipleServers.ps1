20d02822ed0446bb026dd33e0b22d4554e730e0f
chkiran248 <chkiran248@gmail.com>
2019-06-11 17:04:56 +0200

    lastreboottime

---
+++ WindowsPS/LastRebootTimeMultipleServers.ps1
@@ -0,0 +1,8 @@
+﻿function Get-Uptime ($target)
+{
+
+Get-WmiObject win32_operatingsystem -Computername $target -Credential $MyCred -Authentication Default | select csname, @{LABEL=’LastBootUpTime’;EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}}
+
+}
+
+Get-Uptime localhost,server1,server2
