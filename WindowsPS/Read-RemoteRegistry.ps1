83632e754556c0aaaa561a1c08eeff34fe9ef49f
chkiran248 <chkiran248@gmail.com>
2019-06-11 16:49:00 +0200

    reg

---
+++ WindowsPS/Read-RemoteRegistry.ps1
@@ -0,0 +1,15 @@
+﻿$key="SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters"
+$valuename="HostName"
+$type = [Microsoft.Win32.RegistryHive]::LocalMachine
+$computers = Get-Content c:\servers.txt
+foreach($computer in $computers)
+{
+    $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($type,$computer)
+    
+    #write $reg
+    $regkey = $reg.opensubkey($key)
+    $v= $regkey.getvalue($valuename)
+    Write-Host -nonewline $computer `t $v `n
+
+    
+}
