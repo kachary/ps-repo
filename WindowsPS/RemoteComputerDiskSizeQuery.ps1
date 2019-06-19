17056c94004b39edcc3c4e47e84845f10a546066
chkiran248 <chkiran248@gmail.com>
2019-06-19 19:41:08 +0530

    update

--- WindowsPS/RemoteComputerDiskSizeQuery.ps1
+++ WindowsPS/RemoteComputerDiskSizeQuery.ps1
@@ -1,7 +1,6 @@
 <# Using WMIquery#>
 
-Get-WmiObject Win32_LogicalDisk -ComputerName Localhost -Filter "DeviceID='C:'" | select Name,FileSystem,Freespace,Size | 
-Format-Table Name, 
+Get-WmiObject Win32_LogicalDisk -ComputerName ftc-vdi10d01561 -Filter "DeviceID='C:'" | select Name,
              @{Name='FileSystem';Expression={$_.FileSystem}},
              @{Name='FreeSpace';Expression={'{0:N2}' -f ($_.FreeSpace/1GB)}},
-             @{Name='TotalSize';Expression={'{0:N2}' -f ($_.Size/1GB)}}
+             @{Name='TotalSize';Expression={'{0:N2}' -f ($_.Size/1GB)}} | Format-Table
